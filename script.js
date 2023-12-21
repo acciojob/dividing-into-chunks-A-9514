ebeac92a091d18c69e2cc6dcbe01c5205956aec9
Aishwarya <142329750+A-9514@users.noreply.github.com>
2023-12-21 20:19:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-	
-  let ans = [];
-  let sum = 0;
-  let a = [];
-  for(let i of arr){
-	  
-	  sum += i;
-	  a.push(i);
-
-	  if(sum > n){
-		  a.pop();
-		  ans.push(a);
-		  a = [];
-          a.push(i);
-		  sum = i;
-	  }
-  }	
-
-  if(a.length != 0)ans.push(a);
-
+  // Write your code here
+	let subarray = [],ans=[],maxsum=0;
+	for(let i=0;i<arr.length;i++){
+		if(maxsum+arr[i] <= n){
+			maxsum+=arr[i];
+			subarray.push(arr[i]);
+		}
+		else{
+			ans.push(subarray);
+			maxsum=arr[i];
+			subarray[arr[i]];
+		}
+	}
 	return ans;
 };
-
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
